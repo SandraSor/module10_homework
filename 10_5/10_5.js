941c084be3e82af1c152efdce53e89e6a640e302
Anastasiya Sorokina <38187366+SandraSor@users.noreply.github.com>
2023-08-08 14:42:09 +0300

    Add localStorage

--- 10_5/10_5.js
+++ 10_5/10_5.js
@@ -4,52 +4,60 @@ const limit=document.querySelector('.limit');
 const btn= document.querySelector('.btn_req');
 const result= document.querySelector('.result');
 
-const fetchData = () => {
-  return fetch(`https://picsum.photos/v2/list?page=${page.value}&limit=${limit.value}`)
+const fetchData = (page, limit) => {
+  return fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`)
   .then(res => return res.json())
   .then(json => return json)
   .catch(() => { console.log('error') });
 }
 
-btn.addEventListener('click', ()=>{
-  let div = document.createElement('div');
-  div.className = "alert";
+function showResult(apiData) {
+    let cards = '';
+    apiData.forEach(item => {
+        const cardBlock = `
+            <div class="card">
+                <img class="card-image" src="${item.download_url}">
+                <p>${item.author}</p>
+            </div>
+        `;
+        cards += cardBlock;
+    });
+
+    result.innerHTML = cards;
+    result.style.display = 'flex';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    storageItem = localStorage.getItem('lastResponse')
+    if (storageItem) {
+        showResult(JSON.parse(storageItem));
+    }
+});
+
+btn.addEventListener('click', async ()=>{
+  let elem = document.createElement('p');
+  elem.className = "alert";
   let flag=true;
   if((page.value <1) || (page.value >10)){
-    div.innerHTML = "Номер страницы вне диапазона от 1 до 10";
+    elem.innerHTML = "Номер страницы вне диапазона от 1 до 10";
+    result.append(elem);
     flag=false;
   }
   if((limit.value <1) || (limit.value >10)){
-    div.innerHTML = "Лимит вне диапазона от 1 до 10";
+    elem.innerHTML = "Лимит вне диапазона от 1 до 10";
+    result.append(elem);
     flag=false;
   }
   if(((page.value <1) || (page.value >10)) && ((limit.value <1) || (limit.value >10))){
-    div.innerHTML = "Номер страницы и лимит вне диапазона от 1 до 10";
+    elem.innerHTML = "Номер страницы и лимит вне диапазона от 1 до 10";
+    result.append(elem);
     flag=false;
   }
-  wrapper.after(div);
   
   if(flag){
-    const requestResult = await fetchData();
+    const requestResult = await fetchData(page.value, limit.value);
     console.log('requestResult', requestResult);
-    let cards = '';
-  // console.log('start cards', cards);
-  
-  requestResult.forEach(item => {
-    const cardBlock = `
-      <div class="card">
-        <img
-          src="${item.download_url}"
-          class="card-image"
-        />
-        <p>${item.author}</p>
-      </div>
-    `;
-    cards = cards + cardBlock;
-  });
-  
-  // console.log('end cards', cards);
-    
-  result.innerHTML = cards;
+    localStorage.setItem('lastResponse', JSON.stringify(requestResult));
+    showResult(requestResult);
   }
 })
